2ffb4a617c5314aa00ba9a49e77c62097cbd41e5
Victor Pedro <victor@victorpedro.me>
2023-04-24 23:38:09 -0300

    add nvim-tree file tree plugin

--- init.lua
+++ init.lua
@@ -35,6 +35,14 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {},
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
